abcbb5873acac25f39819a226175315967338d83
DngDavide02 <dangelodavide02@gmail.com>
2025-07-18 09:15:29 +0200

    Initial commit

---
+++ U2-W2-D5/src/test/java/davidedangelo/U2_W2_D5/U2W2D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package davidedangelo.U2_W2_D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
